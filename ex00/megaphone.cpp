4f6aaace8ab32e9acde1d709bdaaf4c5370dcc97
xbeheydt <xavier.beheydt@gmail.com>
2022-04-14 10:44:39 +0200

    initial commit

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -15,18 +15,18 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc > 1)
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            for (char *str = argv[i]; *str != '\0'; str++)
-                std::cout << static_cast<char>(std::toupper(*str));
-        }
-    }
-    else
-    {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    }
-    std::cout << std::endl;
-    return (0);
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (char *str = argv[i]; *str != '\0'; str++)
+				std::cout << static_cast<char>(std::toupper(*str));
+		}
+	}
+	else
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	std::cout << std::endl;
+	return (0);
 }
